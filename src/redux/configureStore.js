4a97592a2d37989c064ddbb61d595426a0a9f507
hsangwon <101050184+hsangwon@users.noreply.github.com>
2022-05-03 15:43:22 +0900

    feat: 북마크&버그POST

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,16 +3,15 @@ import thunk from "redux-thunk";
 import { createBrowserHistory } from "history";
 import { connectRouter } from "connected-react-router";
 import User from "./modules/user";
-// import Post from "./modules/post";
+import Post from "./modules/post";
 // import Image from "./modules/image";
-
 
 export const history = createBrowserHistory();
 
 const rootReducer = combineReducers({
   user: User,
-//   post: Post,
-//   image: Image,
+  post: Post,
+  //   image: Image,
   router: connectRouter(history),
 });
 
@@ -23,7 +22,7 @@ const env = process.env.NODE_ENV;
 if (env === "development") {
   const { logger } = require("redux-logger");
   middlewares.push(logger);
-};
+}
 
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
